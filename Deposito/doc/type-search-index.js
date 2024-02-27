ebf953ad15953a41b25c0aab5551dbbc85a6fd31
racielalexa <racielalexa16@gmail.com>
2024-02-27 22:57:27 +0100

    Commit documentacion JAVADOC

---
+++ Deposito/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cuentas","l":"cuenta1"}];updateSearchResults();
--- Deposito/src/cuentas/cuenta1.java
+++ Deposito/src/cuentas/cuenta1.java
@@ -1,16 +1,27 @@
 package cuentas;
 public class cuenta1 {
 
+	/**
+	 * @author racie
+	 * Clase cuenta
+	 * */
 
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    private double tipoInteres;
 
     public cuenta1()
     {
     }
 
+  /**
+   * Parametrizacion de la clase cuenta
+   * @param nom
+   * @param cue
+   * @param sal
+   * @param tipo
+   */
     public cuenta1(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -18,51 +29,87 @@ public class cuenta1 {
         saldo=sal;
     }
     
-
+/**
+ * Se obtiene el nombre que tendra la cuenta
+ * @return
+ */
     public String getNombre() {
 		return nombre;
 	}
-
+/**
+ * devuelve el nombre obtenido en el getter
+ * @param nombre
+ */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+/**
+ * Se obtiene la cuenta 
+ * @get
+ */
 	public String getCuenta() {
 		return cuenta;
 	}
-
+/**
+ * devuelve la cuenta
+ * @return cuenta
+ * 
+ */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
-
+/**
+ * Obtiene el saldo que tiene la cuenta
+ * @return
+ */
 	public double getSaldo() {
 		return saldo;
 	}
-
+/**
+ * Devuelve la cuenta oobtenida en el metodo getSaldo
+ * @return saldo
+ */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-
-	public double getTipoInterés() {
-		return tipoInterés;
+/**
+ * Obtiene el tipo de interes de la cuenta
+ * @return
+ */
+	public double getTipoInteres() {
+		return tipoInteres;
+	}
+/**
+ * Devuelve el tipo de interes
+ * @param tipoInteres
+ */
+	public void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
 	}
 
-	public void setTipoInterés(double tipoInterés) {
-		this.tipoInterés = tipoInterés;
-	}
-
+	/**
+	 * @see saldo en cuenta
+	 */
 	public double estado()
     {
         return saldo;
     }
-
+/**
+ * Ingresar cantidad en la cuenta
+ * @param cantidad
+ * @throws Exception no se puede ingresar cantidad negativa
+ */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
-
+/**
+ * 
+ * @param cantidad a retirar
+ * @throws Exception no se puede retirar cantidad negativa y si no hay suficiente cantidad a ingresar
+ */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
